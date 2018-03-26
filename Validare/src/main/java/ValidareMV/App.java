f80391dc975170df4fd79c990a4fb290dc5b70d0
ionitaflorinc <33004642+ionitaflorinc@users.noreply.github.com>
2018-03-26 20:34:04 +0300

    Added project

---
+++ Validare/src/main/java/ValidareMV/App.java
@@ -0,0 +1,13 @@
+package ValidareMV;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
